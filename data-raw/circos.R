aeba68e0edd933351a503e556b9a3e93cacf06ac
Romain Francois <romain@thinkr.fr>
2017-06-13 06:29:39 +0200

    mutate

--- data-raw/circos.R
+++ data-raw/circos.R
@@ -3,4 +3,12 @@ library(dplyr)
 library(geojsonio)
 
 circos <- geojson_read( "data-raw/france-circonscriptions-legislatives-2012.json", method = "local", what = "sp")
+
+circos@data <- mutate( circos@data,
+  num_circ = as.numeric(num_circ),
+  code_dpt = as.character(code_dpt),
+  ID = as.character(ID),
+  nom_reg = as.character(nom_reg),
+  code_reg = as.character(code_reg)
+)
 use_data( circos, overwrite = TRUE )
