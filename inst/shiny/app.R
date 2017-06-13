bb408fc242b36f32a7bd013a944682abf4481c40
Romain Francois <romain@thinkr.fr>
2017-06-13 06:53:24 +0200

    carte de l'abstention

--- inst/shiny/app.R
+++ inst/shiny/app.R
@@ -1,19 +1,40 @@
 library(shiny)
 library(leaflet)
 library(legislatives2017)
+library(dplyr)
+library(stringr)
+library(htmltools)
+library(purrr)
 
-ui <- fluidPage(
-  div( style = "position: fixed; top: 0; bottom: 0; right:0; left:0; ",
-    leafletOutput( "carte", width="100%", height="100%" )
+ui <- navbarPage( "Legislatives 2017", theme = "legislatives.css",
+
+  tabPanel("Abstention",
+    div( class = "fullpage",
+      leafletOutput( "carte_abstention", width="100%", height="100%" )
+    )
   )
+
 )
 
 server <- shinyServer(function(input, output){
-  output$carte <- renderLeaflet({
+
+  data_abstention <- premier_tour %>%
+    distinct(dpt, circ, .keep_all = TRUE) %>%
+    select(dpt, circ, Inscrits:Exprimes) %>%
+    left_join( circos@data, ., by = c( code_dpt = "dpt", num_circ = "circ")) %>%
+    mutate( p_abstention = Abstentions / Inscrits )
+
+  output$carte_abstention <- renderLeaflet({
+    abst <- data_abstention$p_abstention
+    col <- gray( 1 - ( abst - min(abst) ) / ( max(abst) - min(abst) ) )
+
+    labels <- with( data_abstention, sprintf( "%s (circonscription %d) <hr/>%d inscrits<br/>%d abstentions (%4.2f %%)", str_to_title(nom_dpt), num_circ, Inscrits, Abstentions, round(100*p_abstention, 2 ) )) %>%
+      map(HTML)
+
     leaflet(circos) %>%
       addTiles( urlTemplate = 'http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png' ) %>%
-      setView(lng = 5, lat= 45, zoom=5) %>%
-      addPolygons( color = "black", weight = 1, fillColor = "black", fill = TRUE, fillOpacity = 0.6,
+      setView(lng = 5, lat= 47, zoom=6) %>%
+      addPolygons( color = "black", weight = 1, fillColor = col, fill = TRUE, fillOpacity = 1, label = labels,
         highlightOptions = highlightOptions(weight = 2, fillOpacity = 1, bringToFront = TRUE),
         labelOptions = labelOptions(
           style = list("font-weight" = "normal", padding = "3px 8px"),
@@ -22,6 +43,8 @@ server <- shinyServer(function(input, output){
         )
       )
   })
+
+
 })
 
 shinyApp( ui = ui, server = server )
