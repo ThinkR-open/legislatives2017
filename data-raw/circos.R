adc4c9ded2b1eeb1cbdfb46eb582df0f40658d94
Romain Francois <romain@thinkr.fr>
2017-06-13 06:32:40 +0200

    as_tibble

--- data-raw/circos.R
+++ data-raw/circos.R
@@ -1,14 +1,15 @@
 library(readr)
 library(dplyr)
 library(geojsonio)
+library(tibble)
 
 circos <- geojson_read( "data-raw/france-circonscriptions-legislatives-2012.json", method = "local", what = "sp")
 
-circos@data <- mutate( circos@data,
-  num_circ = as.numeric(num_circ),
+circos@data <- as_tibble(mutate( circos@data,
+  num_circ = as.numeric(as.character(num_circ)),
   code_dpt = as.character(code_dpt),
   ID = as.character(ID),
   nom_reg = as.character(nom_reg),
   code_reg = as.character(code_reg)
-)
+))
 use_data( circos, overwrite = TRUE )
