8ca4b0b36be6778a2f75e52396c6a938be6bab34
Romain Francois <romain@thinkr.fr>
2017-06-19 19:07:31 +0200

    table de correspondance rgsion dpt

--- data-raw/circos.R
+++ data-raw/circos.R
@@ -16,3 +16,22 @@ circos@data <- as_tibble(mutate( circos@data,
   nom_reg = str_to_title(nom_reg)
 ))
 use_data( circos, overwrite = TRUE )
+
+expat <- data_frame(
+  code_dpt = "099",
+  nom_dpt = "Français établis hors de France",
+  code_reg = "099",
+  nom_reg = "Français établis hors de France"
+)
+
+regions <- circos@data %>%
+  select( code_dpt, nom_dpt, nom_reg, code_reg ) %>%
+  distinct(code_dpt, code_reg, .keep_all = TRUE) %>%
+  bind_rows( expat ) %>%
+  mutate(
+    nom_reg = str_replace_all( nom_reg, "[-]", " "),
+    nom_dpt = str_replace_all( nom_dpt, "[-]", " ")
+  )
+
+use_data( regions, overwrite = TRUE )
+
